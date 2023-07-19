adeb1752b59f2b0806aea249af2850ab5b3e9f79
BrookeBryski <brookeabryski@gmail.com>
2023-07-19 16:03:23 -0700

    method override;

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
 
 const Product = require('./models/product');
 
@@ -16,7 +18,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/products', async (req, res) => {
    const products =  await Product.find({});
@@ -39,6 +43,13 @@ app.get('/products/:id', async (req, res) => {
     res.render('products/show', { product })
 })
 
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res))
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000")
