26b0c8772aa55945f0c3162102fc1c09f72a8465
BrookeBryski <brookeabryski@gmail.com>
2023-07-19 15:43:08 -0700

    new product

--- index.js
+++ index.js
@@ -16,10 +16,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/products', async (req, res) => {
    const products =  await Product.find({});
    res.render('products/index', { products })
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`)
 })
 
 app.get('/products/:id', async (req, res) => {
@@ -27,6 +38,7 @@ app.get('/products/:id', async (req, res) => {
     const product = await Product.findById(id);
     res.render('products/show', { product })
 })
+
 
 app.listen(3000, () => {
     console.log("App is listening on port 3000")
